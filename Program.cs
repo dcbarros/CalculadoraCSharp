f2f064148915e19e82b725945bc6a3b6ccbab387
dcbarros <dcbarros1@gmail.com>
2022-02-07 20:20:47 -0300

    new function

--- Program.cs
+++ Program.cs
@@ -4,13 +4,105 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 
-namespace MyApp // Note: actual namespace depends on the project name.
+namespace Calculator
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello ");
+            bool valChange = true;
+
+            while (valChange)
+            {
+                Console.Clear();
+                System.Console.WriteLine("Coloque o primeiro valor: ");
+                float value1 = float.Parse(Console.ReadLine());
+                System.Console.WriteLine("Coloque o segundo valor: ");
+                float value2 = float.Parse(Console.ReadLine());
+
+                short choice = Menu();
+
+                switch (choice)
+                {
+                    case 1:
+                        {
+                            Soma(value1, value2);
+                            break;
+                        }
+                    case 2:
+                        {
+                            Subtracao(value1, value2);
+                            break;
+                        }
+                    case 3:
+                        {
+                            Multipicacao(value1, value2);
+                            break;
+                        }
+                    case 4:
+                        {
+                            Divicao(value1, value2);
+                            break;
+                        }
+                    case 5:{
+                        Console.Clear();
+                        System.Console.WriteLine("Obrigado!");
+                        valChange = false;
+                        break;
+                    }
+                    default:
+                        Menu();
+                        break;
+                }
+            }
+        }
+
+        static short Menu()
+        {
+
+            System.Console.Clear();
+            System.Console.WriteLine("------------------------------------------------------------------------------");
+            System.Console.WriteLine("1 - Soma; 2 - Subtração; 3 - Multiplicação; 4 - Divisão; 5 - Sair da execução");
+            System.Console.WriteLine("Digite a operação que quer ser realizada: ");
+            short choice = short.Parse(Console.ReadLine());
+            System.Console.WriteLine("------------------------------------------------------------------------------");
+            return choice;
+        }
+
+        static void Soma(float value1, float value2)
+        {
+            Console.Clear();
+            System.Console.WriteLine("-----------------------");
+            System.Console.WriteLine($"O resultado da soma de {value1} + {value2} é: {value1 + value2}");
+            System.Console.WriteLine("-----------------------");
+            Console.ReadKey();
+        }
+
+        static void Subtracao(float value1, float value2)
+        {
+            Console.Clear();
+            System.Console.WriteLine("-----------------------");
+            System.Console.WriteLine($"O resultado da subtração de {value1} - {value2} é: {value1 - value2}");
+            System.Console.WriteLine("-----------------------");
+            Console.ReadKey();
+        }
+
+        static void Multipicacao(float value1, float value2)
+        {
+            Console.Clear();
+            System.Console.WriteLine("-----------------------");
+            System.Console.WriteLine($"O resultado da multiplicação de {value1} * {value2} é: {value1 * value2}");
+            System.Console.WriteLine("-----------------------");
+            Console.ReadKey();
+        }
+
+        static void Divicao(float value1, float value2)
+        {
+            Console.Clear();
+            System.Console.WriteLine("-----------------------");
+            System.Console.WriteLine($"O resultado da divição de {value1} / {value2} é: {value1 / value2}");
+            System.Console.WriteLine("-----------------------");
+            Console.ReadKey();
         }
     }
 }
